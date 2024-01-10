f6700b19fed950258914f1ccb74c2e9f9044c9df
Jeffery Mutuku <jefferymutuku283@gmail.com>
2024-01-10 18:17:14 +0300

    Project 3

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int _isdigit(int c)
+{
+    if (c >= 18 && c <= 57)
+        return 1;
+    else
+        return 0;
+}
---
+++ 0x04-more_functions_nested_loops/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+long number = 612852475143;
+    long result = largest_prime_factor(number);
